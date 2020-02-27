5e81d79911031a586579a2233f75b23a4d09f39c
rudalson <rudalson@gmail.com>
2020-02-28 01:35:15 +0900

    added the getting the current gps by expo api
    loading 때 expo api를 통해 gps 정보를 구해오는 것을 구현.
    다만 안드로이드는 expo의 Location.getCurrentPositionsAsync() 에서 문제가 있어서 option 조정을 하였다. 수정의 근거는 https://github.com/expo/expo/issues/5504 을 통하여 하였다.

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import {Platform, Alert} from "react-native";
 import Loading from "./Loading";
 import * as Location from 'expo-location';
 
 export default class App extends React.Component {
-  getLocation = async() => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
-  };
+    state = {
+        isLoading: true,
+    };
 
-  componentDidMount() {
-    this.getLocation();
-  }
-  render() {
-    return (
-        <Loading/>
-    );
-  }
+    getLocation = async () => {
+        try {
+            await Location.requestPermissionsAsync();
+
+            let options = {};
+            if (Platform.OS === 'android') {
+                options = {accuracy: Location.Accuracy.High};
+            }
+
+            const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync(options);
+            console.log(latitude, longitude);
+            this.setState({isLoading: false});
+        } catch (error) {
+            Alert.alert("Can't find you", "So sad");
+            console.error(error);
+        }
+    };
+
+    componentDidMount() {
+        this.getLocation();
+    }
+
+    render() {
+        const {isLoading} = this.state;
+        return (
+            isLoading ? <Loading/> : null
+        );
+    }
 }
 
