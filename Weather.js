8a19106c394c06701f03a561027b46d706ba3f57
rudalson <rudalson@gmail.com>
2020-03-01 13:10:02 +0900

    added the bottom-half text info
    하단에 날씨를 표현하는 텍스트 내용 출력 추가

--- Weather.js
+++ Weather.js
@@ -7,47 +7,69 @@ import {LinearGradient} from 'expo-linear-gradient';
 const weatherOptions = {
     Thunderstorm: {
         iconName: "weather-lightning",
-        gradient: ["#373B44", "#4286f4"]
+        gradient: ["#373B44", "#4286f4"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Drizzle: {
         iconName: "weather-hail",
-        gradient: ["#89F7FE", "#66A6FF"]
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Rain: {
         iconName: "weather-rainy",
-        gradient: ["#00C6FB", "#005BEA"]
+        gradient: ["#00C6FB", "#005BEA"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Snow: {
         iconName: "weather-snowy",
-        gradient: ["#7DE2FC", "#B9B6E5"]
+        gradient: ["#7DE2FC", "#B9B6E5"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Atmosphere: {
         iconName: "weather-hail",
-        gradient: ["#89F7FE", "#66A6FF"]
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Clear: {
         iconName: "weather-sunny",
-        gradient: ["#FF7300", "#FEF253"]
+        gradient: ["#FF7300", "#FEF253"],
+        title: '맑음',
+        subtitle: '맑아요',
     },
     Clouds: {
         iconName: "weather-cloudy",
-        gradient: ["#D7D2CC", "#304352"]
+        gradient: ["#D7D2CC", "#304352"],
+        title: '흐림',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Mist: {
         iconName: "weather-hail",
-        gradient: ["#4DA0B0", "#D39D38"]
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Dust: {
         iconName: "weather-hail",
-        gradient: ["#4DA0B0", "#D39D38"]
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Haze: {
         iconName: "weather-hail",
-        gradient: ["#4DA0B0", "#D39D38"]
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
     Fog: {
         iconName: "weather-hail",
-        gradient: ["#4DA0B0", "#D39D38"]
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: '안개',
+        subtitle: '구름이 많고 날씨가 흐립니다',
     },
 };
 
@@ -61,8 +83,9 @@ export default function Weather({temp, condition}) {
                 <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={96} color="white"/>
                 <Text style={styles.temp}>{temp}°C</Text>
             </View>
-            <View style={styles.halfContainer}>
-                <Text>{condition}</Text>
+            <View style={{...styles.halfContainer, ...styles.textContainer}}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
             </View>
         </LinearGradient>
     );
@@ -98,5 +121,19 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     },
-
+    title: {
+        color: 'white',
+        fontSize: 44,
+        fontWeight: "300",
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontWeight: "600",
+        color: 'white',
+        fontSize: 24,
+    },
+    textContainer: {
+        paddingHorizontal: 20,
+        alignItems: "flex-start",
+    }
 });
